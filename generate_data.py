ff2563bb107e44eb6f7ef82b196673196e92fe17
arturo <al102964@gmail.com>
2020-02-13 01:38:52 -0600

    ajuste a generate_data

--- generate_data.py
+++ generate_data.py
@@ -3,14 +3,14 @@ import argparse
 
 def parse_arguments():
     """Manage input arguments for program"""
-    ap = argparse.ArgumentParser()    
-    ap.add_argument("-n", "--samples", required=True, help="first operand")    
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-n", "--samples", required=True, help="first operand")
     ap.add_argument("-o", "--output_file", required=True, help="second operand")
     args = vars(ap.parse_args())
     return args
 
 def main(args):
-    """Main function, generate data"""    
+    """Main function, generate data"""
     nationalities = ["english","russian","french","spanish","italian","swedish","colombian",\
         "mexican","canadian","costarican","argentinian","brazilian","japanese","chinese",\
         "korean","vietnamese","taiwanese","mongolian"]
@@ -23,8 +23,8 @@ def main(args):
             zcode = randint(10000,19000)
             nationality = nationalities[randint(0,len(nationalities)-1)]
             condition = conditions[randint(0,len(conditions)-1)]
-            gender = gender[randint(0,len(gender)-1)]
-            f.write("{},{},{},{},{}\n".format(zcode,nationality,condition,gender,randint(10,80)))
+            gen = gender[randint(0,len(gender)-1)]
+            f.write("{},{},{},{},{}\n".format(zcode,nationality,condition,gen,randint(10,80)))
 
 if __name__ == '__main__':
     arguments = parse_arguments()
